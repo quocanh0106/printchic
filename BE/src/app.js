26709f4dea46aac6fc79133d68bd628474818942
lynvu99 <thangvd1@runsystem.net>
2024-02-03 19:00:55 +0700

    change port api

--- BE/src/app.js
+++ BE/src/app.js
@@ -47,7 +47,7 @@ const normalizePort = (val) => {
     return false;
 };
 
-const port = normalizePort(process.env.PORT || '5000');
+const port = normalizePort(process.env.PORT || '7000');
 
 server.listen(port, () => {
     console.log(`App is listening at ${port}`);
