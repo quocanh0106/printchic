c5fcbb7edc5fb71c4f723ee964a27df96c90c5a2
lynvu99 <thangvd1@runsystem.net>
2024-02-08 12:11:16 +0700

    add library

---
+++ USER/components/blog.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="blog-wrapper flex">
+    <div class="blog-left-wrapper flex flex-col">
+        <img :src="listBlog[0]?.imgUrl" />
+        <h1>{{ listBlog[0]?.title }}</h1>
+        <p>{{ listBlog[0]?.description }}</p>
+        <p>{{ listBlog[0]?.createdAt }}</p>
+    </div>
+    <div class="blog-right-wrapper flex flex-col" v-for="(blog, index) in listBlog" :key="index">
+        <div class="side-blog-wrapper flex"  v-if="index <=3 && index > 0">
+            <img :src="blog.imgUrl" />
+            <div>
+                <h1>{{ blog.title }}</h1>
+                <p>{{ blog.description }}</p>
+                <p>{{ blog.createdAt }}</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            listBlog: [
+                {
+                    imgUrl: '"https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png"',
+                    title: 'Demi Moore gives update on Bruce Willis',
+                    description: 'Nowhere are these attacks more evident right now than on college campuses. Many academics who teach about the history of race and racism in America, as I do, are being unjustifiably blamed.',
+                    createdAt: 'April 11, 2023'
+                },
+                {
+                    imgUrl: '"https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png"',
+                    title: 'Demi Moore gives update on Bruce Willis',
+                    description: 'Nowhere are these attacks more evident right now than on college campuses. Many academics who teach about the history of race and racism in America, as I do, are being unjustifiably blamed.',
+                    createdAt: 'April 11, 2023'
+                },
+                {
+                    imgUrl: '"https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png"',
+                    title: 'Demi Moore gives update on Bruce Willis',
+                    description: 'Nowhere are these attacks more evident right now than on college campuses. Many academics who teach about the history of race and racism in America, as I do, are being unjustifiably blamed.',
+                    createdAt: 'April 11, 2023'
+                },
+                {
+                    imgUrl: '"https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png"',
+                    title: 'Demi Moore gives update on Bruce Willis',
+                    description: 'Nowhere are these attacks more evident right now than on college campuses. Many academics who teach about the history of race and racism in America, as I do, are being unjustifiably blamed.',
+                    createdAt: 'April 11, 2023'
+                }
+            ]
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
--- USER/components/carousel.vue
+++ USER/components/carousel.vue
@@ -1,30 +1,91 @@
 <template>
   <div class="carousel">
-    <v-carousel :items="items" :items-per-skew="4" :cycle="true" :interval="3000" :show-arrows="false">
-      <v-carousel-item v-for="item in items" :key="item.id" >
-        <v-img :src="item.image" height="200px" width="200px" />
-        <v-card-text>{{ item.title }}</v-card-text>
-      </v-carousel-item>
+    <v-carousel :show-arrows="false">
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+      ></v-carousel-item>
     </v-carousel>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       items: [
-        { id: 1, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 1" },
-        { id: 2, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 2" },
-        { id: 3, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 3" },
-        { id: 4, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 4" },
-        { id: 5, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 5" },
-        { id: 6, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 6" },
-        { id: 7, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 7" },
-        { id: 8, image: "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png", title: "Item 8" },
+        {
+          id: 1,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 1",
+        },
+        {
+          id: 2,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 2",
+        },
+        {
+          id: 3,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 3",
+        },
+        {
+          id: 4,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 4",
+        },
+        {
+          id: 5,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 5",
+        },
+        {
+          id: 6,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 6",
+        },
+        {
+          id: 7,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 7",
+        },
+        {
+          id: 8,
+          image:
+            "https://casualday.co.za/wp-content/uploads/2020/10/Kids-TShirt-2023.png",
+          title: "Item 8",
+        },
       ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .carousel {
+    :deep(.v-carousel__controls){
+      background-color: transparent !important;
+    }
+    :deep(.v-btn__overlay){
+      width: 0px ;
+      height: 0px;
+    }
+  }
+</style>
