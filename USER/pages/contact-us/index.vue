cab42f9c2a3af923faddc855e3281f7d7e989579
lynvu99 <thangvd1@runsystem.net>
2024-02-25 23:29:33 +0700

    fix ui

--- USER/pages/contact-us/index.vue
+++ USER/pages/contact-us/index.vue
@@ -1,103 +1,190 @@
 <template>
-    <div class="contact-us-page-wrapper">
-
-      <!-- contact form -->
-      <div class="contac-form-wrapper flex justify-between items-center custom-padding">
-        <div class="contact-form">
-          form
+  <div class="contact-us-page-wrapper">
+    <!-- contact form -->
+    <div
+      class="contact-form-wrapper flex justify-between items-start custom-padding"
+    >
+      <div class="contact-form w-1/2">
+        <h1 class="font-semibold title-contact">
+          {{ $t("contactUs.contactUs") }}
+        </h1>
+        <div class="form-contact mt-8">
+          <form @submit.prevent="submitForm" class="">
+            <div class="flex flex-col">
+              <label class="label-form" for="name">{{
+                $t("contactUs.yourName")
+              }}</label>
+              <input
+                :placeholder="$t('contactUs.fillYourName')"
+                class="input-contact mt-1.5"
+                type="text"
+                id="name"
+                v-model="form.name"
+              />
+            </div>
+            <div class="flex flex-col mt-6">
+              <label class="label-form" for="email">{{
+                $t("contactUs.yourEmail")
+              }}</label>
+              <input
+                :placeholder="$t('contactUs.fillYourName')"
+                class="input-contact mt-1.5"
+                type="password"
+                id="email"
+                v-model="form.email"
+              />
+            </div>
+            <div class="flex flex-col mt-6">
+              <label class="label-form" for="message">{{
+                $t("contactUs.yourMessage")
+              }}</label>
+              <input
+                :placeholder="$t('contactUs.fillYourMessage')"
+                class="input-contact mt-1.5"
+                type="password"
+                id="message"
+                v-model="form.message"
+              />
+            </div>
+            <div class="action-contact-button flex flex-col mt-6">
+              <button type="submit" class="primary-btn text-white cursor-pointer">
+                {{
+                $t("contactUs.submit")
+              }}
+              </button>
+              <div class="relative text-center mt-6">
+                <div
+                  class="absolute inset-0 flex items-center"
+                  aria-hidden="true"
+                >
+                  <div class="w-full border-t border-gray-300"></div>
+                </div>
+                <div class="relative flex justify-center">
+                  <span class="px-3 bg-white text-sm text-gray-500">or</span>
+                </div>
+              </div>
+              <button type="submit" class="secondary-btn mt-6 cursor-pointer">{{
+                $t("contactUs.contactViaMessenger")
+              }}</button>
+            </div>
+          </form>
         </div>
-        <img class="rounded-lg" :src="contactThumb"/>
       </div>
-      <!-- social -->
-      <div class="social-wrapper bg-light-gray-custom custom-padding">
-        <div class="flex gap-x-8">
-            <div class="motive-body-left flex-1 flex-col justify-start">
-              <h5 class="txt-primary font-semibold border-bottom-primary pb-3">
-                {{ $t("contactUs.chatToUs") }}
-              </h5>
-              <span class="mt-3">
-                <div class="mt-5">
-                  <p>Mon - Fri : 9h00 - 18h00</p>
-                </div>
-                <div class="">
-                  <p>8h30am – 5h30pm (UTC+07:00)</p>
-                </div>
-                <div class="flex mt-3">
-                  <img :src="mailIcon" />
-                  <p class="ml-1.5">contact@printchic.com</p>
-                </div>
-                <div class="mt-3 flex gap-x-1.5 cursor-pointer">
-                  <a class="txt-primary font-semibold">{{ $t('contactUs.contactViaMessenger') }}</a>
-                  <img :src="arrowUpRight" />
-                </div>
-              </span>
-            </div>
-            <div class="motive-body-left flex-1 flex-col">
-              <h5 class="font-semibold border-bottom-primary pb-3">
-                {{ $t("contactUs.office") }}
-              </h5>
-              <p class="mt-3">
-                UK Office :
-              </p>
-              <p>
-                23 Old Gloucester Street, WC1N 3AX,
-              LONDON, United Kingdom
-              </p>
-            </div>
-            <div class="motive-body-left flex-1 flex-col">
-              <h5 class="font-semibold border-bottom-primary pb-3">
-                {{ $t("contactUs.socialNetwork") }}
-              </h5>
-              <span class="flex mt-3 gap-x-2">
-                <img class="socials" :src="linkedin"/>
-                <img class="socials" :src="facebook"/>
-                <img class="socials" :src="twitter"/>
-              </span>
-              <p class="mt-3">
-                {{ $t("contactUs.reviewOurSupport") }}
-              </p>
-            </div>
-        </div>
-      </div>
-      <!-- help -->
-      <help
-          :headerTitle="$t('homePage.howCanWeHelp')"
-          :headerDesc="$t('homePage.howCanWeHelpDesc')"
-          class="custom-padding"
-      />
+      <img class="w-1/2 rounded-lg" :src="contactThumb" />
     </div>
+    <!-- social -->
+    <div class="social-wrapper bg-light-gray-custom custom-padding">
+      <div class="flex gap-x-8">
+        <div class="motive-body-left flex-1 flex-col justify-start">
+          <h5
+            class="txt-primary text-xl font-semibold border-bottom-primary pb-3"
+          >
+            {{ $t("contactUs.chatToUs") }}
+          </h5>
+          <span class="mt-3">
+            <div class="mt-5">
+              <p>Mon - Fri : 9h00 - 18h00</p>
+            </div>
+            <div class="">
+              <p>8h30am – 5h30pm (UTC+07:00)</p>
+            </div>
+            <div class="flex mt-3">
+              <img :src="mailIcon" />
+              <p class="ml-1.5">contact@printchic.com</p>
+            </div>
+            <div class="mt-3 flex gap-x-1.5 cursor-pointer">
+              <a class="txt-primary font-semibold">{{
+                $t("contactUs.contactViaMessenger")
+              }}</a>
+              <img :src="arrowUpRight" />
+            </div>
+          </span>
+        </div>
+        <div class="motive-body-left flex-1 flex-col">
+          <h5 class="font-semibold border-bottom-primary text-xl pb-3">
+            {{ $t("contactUs.office") }}
+          </h5>
+          <p class="mt-3">UK Office :</p>
+          <p>23 Old Gloucester Street, WC1N 3AX, LONDON, United Kingdom</p>
+        </div>
+        <div class="motive-body-left flex-1 flex-col">
+          <h5 class="font-semibold text-xl border-bottom-primary pb-3">
+            {{ $t("contactUs.socialNetwork") }}
+          </h5>
+          <span class="flex mt-3 gap-x-2">
+            <img class="socials" :src="linkedin" />
+            <img class="socials" :src="facebook" />
+            <img class="socials" :src="twitter" />
+          </span>
+          <p class="mt-3">
+            {{ $t("contactUs.reviewOurSupport") }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- help -->
+    <help
+      :headerTitle="$t('homePage.howCanWeHelp')"
+      :headerDesc="$t('homePage.howCanWeHelpDesc')"
+      class="custom-padding"
+    />
+  </div>
 </template>
 
 <script>
 import help from "../../components/help.vue";
 import mailIcon from "../assets/svg/mail.svg";
-import contactThumb from "../../assets/images/contactUsFormThumb.png"
-import linkedin from "../../assets/svg/LinkedIn.svg"
-import facebook from "../../assets/svg/facebook.svg"
-import twitter from "../../assets/svg/Twitter.svg"
+import contactThumb from "../../assets/images/contactUsFormThumb.png";
+import linkedin from "../../assets/svg/LinkedIn.svg";
+import facebook from "../../assets/svg/facebook.svg";
+import twitter from "../../assets/svg/Twitter.svg";
 import arrowUpRight from "../assets/svg/arrowUpRight.svg";
 
 export default {
-    components: {
-        help,
+  components: {
+    help,
+  },
+  data() {
+    return {
+      mailIcon,
+      contactThumb,
+      linkedin,
+      facebook,
+      twitter,
+      arrowUpRight,
+      form: {},
+    };
+  },
+  methods: {
+    submitForm() {
+      // Process form data here or send data to server
+      console.log(this.form);
+      alert("Form submitted! Check console for details.");
     },
-    data() {
-      return {
-        mailIcon,
-        contactThumb,
-        linkedin,
-        facebook,
-        twitter,
-        arrowUpRight
-      };
-    },
-}
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.socials{
-  background-color: #EFF4FF;
+.socials {
+  background-color: #eff4ff;
   padding: 7px;
   border-radius: 50px;
+}
+.input-contact {
+  border-radius: 6px;
+  border: 1px solid #eaecf0;
+  padding: 10px 12px;
+}
+.label-form {
+  font-size: 14px;
+  font-weight: 600;
+}
+.title-contact {
+  font-size: 40px;
+}
+
+.contact-form-wrapper{
+  gap: 20vw;
 }
 </style>
---
+++ USER/pages/pod/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="pod-page-warpper">
+    <div class="pod-intro-banner">
+      <div
+        class="pod-intro-carousel p-6 bg-white shadow rounded-lg flex items-center justify-between"
+      >
+        <div class="w-100 flex flex-col">
+          <h2 class="text-lg font-semibold current-text">{{ currentText }}</h2>
+          <div class="flex justify-between items-center mt-2">
+            <div class="pagination flex">
+              <span
+                v-for="(item, index) in texts"
+                :key="index"
+                :class="{
+                  'bg-blue-500': currentIndex === index,
+                  'bg-gray-300': currentIndex !== index,
+                }"
+                class="h-2 w-2 mx-1 rounded-full"
+              ></span>
+            </div>
+            <div class="action-btns">
+              <button
+                @click="prev"
+                :disabled="!(currentIndex > 0)"
+                class="text-blue-500 hover:text-blue-700"
+              >
+                prev
+              </button>
+
+              <button
+                :disabled="!(currentIndex < texts.length - 1)"
+                @click="next"
+                class="text-blue-500 hover:text-blue-700"
+              >
+                next
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- fullfillment centers -->
+    <div class="fullfillment-center bg-light-blue-custom">
+      <div class="fullfillment-center-wrapper custom-padding">
+        <div
+          class="fullfillment-center-title flex flex-col justify-center items-center"
+        >
+          <h1 class="section-title font-semibold">
+            {{ $t("contactUs.fullFillMentTitle") }}
+          </h1>
+          <p class="mt-4 mw-584px text-center">
+            {{ $t("contactUs.fullFillMentDes") }}
+          </p>
+        </div>
+        <div class="fullfillment-img flex justify-between mt-12 gap-x-8">
+          <img :src="UK" />
+          <img :src="US" />
+          <img :src="FRANCE" />
+          <img :src="GERMANY" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Vietnam-based Production Capacity -->
+    <div class="production-capacity bg-light-gray1-custom custom-padding">
+      <div class="content">
+        <h1 class="section-title font-semibold">
+          {{ $t("contactUs.vietNameBaseTitle") }}
+        </h1>
+        <p>{{ $t("contactUs.fullFillMentDes") }}</p>
+      </div>
+      <div class="capacity-card-info">
+        <div class="process-card flex flex-col mt-10">
+          <cardInfor
+            :imgSrc="cardThumbnail"
+            :title="$t('homePage.cardTitle1')"
+            :description="$t('homePage.cardDescription1')"
+          />
+          <cardInfor
+            :imgSrc="cardThumbnail"
+            :title="$t('homePage.cardTitle2')"
+            :description="$t('homePage.cardDescription2')"
+          />
+          <cardInfor
+            :imgSrc="cardThumbnail"
+            :title="$t('homePage.cardTitle3')"
+            :description="$t('homePage.cardDescription3')"
+          />
+          <cardInfor
+            :imgSrc="cardThumbnail"
+            :title="$t('homePage.cardTitle4')"
+            :description="$t('homePage.cardDescription4')"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import UK from "../../assets/svg/UK.svg";
+import US from "../../assets/svg/US.svg";
+import FRANCE from "../../assets/svg/FRANCE.svg";
+import GERMANY from "../../assets/svg/GERMANY.svg";
+
+export default {
+  data() {
+    return {
+      UK,
+      US,
+      FRANCE,
+      GERMANY,
+      texts: [
+        "Our Global Fulfillment Centers",
+        "State-of-the-art Logistics",
+        "Worldwide Shipping",
+        "24/7 Customer Support",
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentText() {
+      return this.texts[this.currentIndex];
+    },
+  },
+  methods: {
+    next() {
+      if (this.currentIndex < this.texts.length - 1) {
+        this.currentIndex++;
+      }
+    },
+    prev() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--;
+      }
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.pod-intro-banner {
+  background-image: url("../../assets/images/podCover.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 728px;
+  background-position: bottom right;
+  position: relative;
+}
+
+.pod-intro-carousel {
+  position: absolute;
+  bottom: 60px;
+  right: 120px;
+  height: 149px;
+  width: 20vw;
+  min-width: 20vw;
+  border-top: 7px solid #5c8ee2;
+}
+
+.current-text {
+  min-width: 300px;
+}
+</style>
